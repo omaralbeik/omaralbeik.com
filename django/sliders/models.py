42567d365a616dd9c3d0ead52a449b0eeb36c0b8
Omar Albeik <omaralbeik@gmail.com>
2017-09-30 11:26:57 +0300

    Update sliders order

--- django/sliders/models.py
+++ django/sliders/models.py
@@ -13,7 +13,7 @@ class Slide(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['created_at',]
+        ordering = ['-created_at',]
 
     def __str__(self):
         return self.title
