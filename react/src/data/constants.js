1aa5d91bbd6aa2e3016bc110a3982b0dd01565ca
Omar Albeik <omaralbeik@gmail.com>
2017-10-09 10:03:32 +0300

    Fixes #26

--- react/src/data/constants.js
+++ react/src/data/constants.js
@@ -1,2 +1,9 @@
+// Replace this with your email address
+// used in the contact form
 export const EMAIL = "omaralbeik@gmail.com";
+
+// Replace this with authentication token generated from your Django admin dashboard
 export const API_AUTH_TOKEN = "3eef2d602c9ba76de3dcc7873be07496902c138e";
+
+// Replace this with your Google Analytics tracking number
+export const GA_TRACKING_NUMBER = "UA-107377138-1";
