956a3b56743f24a6018acce4b45bdbbe475923b6
Omar Albeik <omaralbeik@gmail.com>
2017-10-18 15:06:51 +0300

    Fixes #90

--- django/blog/serializers.py
+++ django/blog/serializers.py
@@ -25,4 +25,4 @@ class PostSerializer(serializers.ModelSerializer):
 
     # return post's text as HTML!
     def get_html_text(self, post):
-        return markdown2.markdown(post.text)
+        return markdown2.markdown(post.text, extras=['target-blank-links'])
